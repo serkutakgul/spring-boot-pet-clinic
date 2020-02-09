2e352914909af405c403cbf59a785087d923e03d
serkutakgul <56157543+serkutakgul@users.noreply.github.com>
2020-02-09 16:08:33 +0300

    Added Spring Data Jpa service for owner. Closes #40

--- pet-clinic-data/src/main/java/com/srkt/springbootpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/srkt/springbootpetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,6 @@ import com.srkt.springbootpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner,Long> {
+
+    public Owner findByLastName(String lastName);
 }
